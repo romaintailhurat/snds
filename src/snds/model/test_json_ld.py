7514fb325ec6d861fd4aaff971901422ea4ea25e
romaintailhurat <romain.tailhurat@gmail.com>
2024-07-09 11:54:25 +0000

    Testing variable URN

--- src/snds/model/test_json_ld.py
+++ src/snds/model/test_json_ld.py
@@ -1,13 +1,28 @@
 from snds.model.variable import Variable
 
+import pytest
 
-def test_variable_id_is_there():
-    var = Variable(
+
+@pytest.fixture
+def test_variable_without_urn():
+    return Variable(
         ID="123456789",
         Agency="org.example",
         Version="1",
         URN=None,
         VariableName="TEST_VAR",
     )
-    var_ld = var.to_ld()
+
+
+def test_variable_id_is_there(test_variable_without_urn):
+    var_ld = test_variable_without_urn.to_ld()
     assert "@id" in var_ld.keys()
+
+
+def test_get_urn_from_a_test_variable_without_urn(test_variable_without_urn: Variable):
+    urn = test_variable_without_urn.get_urn()
+    print(urn)
+    assert urn is not None
+    prefix, domain, agency, version, var_id = urn.split(":")
+    assert prefix == "urn"
+    assert domain == "ddi"
