761aa387e9bf3434e6f635c4898fb25fbd0ddade
romaintailhurat <romain.tailhurat@gmail.com>
2024-07-09 11:55:12 +0000

    Clean up

--- src/snds/model/test_json_ld.py
+++ src/snds/model/test_json_ld.py
@@ -20,8 +20,7 @@ def test_variable_id_is_there(test_variable_without_urn):
 
 
 def test_get_urn_from_a_test_variable_without_urn(test_variable_without_urn: Variable):
-    urn = test_variable_without_urn.get_urn()
-    print(urn)
+    urn = test_variable_without_urn.get_urn()    
     assert urn is not None
     prefix, domain, agency, version, var_id = urn.split(":")
     assert prefix == "urn"
