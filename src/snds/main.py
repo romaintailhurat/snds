7d2498c6b7b90edb30d15ba79a0e59c44fa65faf
romaintailhurat <romain.tailhurat@gmail.com>
2024-07-09 15:23:19 +0000

    rye fmt

--- src/snds/main.py
+++ src/snds/main.py
@@ -1,11 +1,13 @@
 import httpx
 import pprint
 
-#resp = httpx.get("https://gitlab.com/healthdatahub/applications-du-hdh/schema-snds/-/raw/master/schemas/DCIR/ER_ANO_F.json?ref_type=heads")
+# resp = httpx.get("https://gitlab.com/healthdatahub/applications-du-hdh/schema-snds/-/raw/master/schemas/DCIR/ER_ANO_F.json?ref_type=heads")
 
-#print(resp.status_code)
+# print(resp.status_code)
 
-resp_tree = httpx.get("https://gitlab.com/api/v4/projects/11935694/repository/tree?path=schemas")
+resp_tree = httpx.get(
+    "https://gitlab.com/api/v4/projects/11935694/repository/tree?path=schemas"
+)
 
 schemas_sub_dirs = [o["path"] for o in resp_tree.json()]
 
